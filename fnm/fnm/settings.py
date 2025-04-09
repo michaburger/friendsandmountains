69101c14dd60e4e34a2bec5bd2ae1f91eb8dd013
Micha Burger <michaburger92@gmail.com>
2025-04-09 18:29:17 +0200

    db strict mode

--- fnm/fnm/settings.py
+++ fnm/fnm/settings.py
@@ -101,6 +101,12 @@ else:
         'default': dj_database_url.config(conn_max_age=600)
     }
     
+    # Add MySQL strict mode for production MySQL database
+    if DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+        DATABASES['default']['OPTIONS'] = {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'",
+        }
+    
     # Prevent makemigrations in production
     def prevent_makemigrations():
         print("Error: makemigrations should not be run in production!")
