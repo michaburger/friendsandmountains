787216b0d1bec92957678d39c2fdd0f0ae1bde04
Micha Burger <michaburger92@gmail.com>
2025-04-09 18:25:05 +0200

    dotenv

--- fnm/fnm/settings.py
+++ fnm/fnm/settings.py
@@ -10,14 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
-import sys
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ENV_PATH = os.path.join(os.path.dirname(BASE_DIR), '.env')
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
